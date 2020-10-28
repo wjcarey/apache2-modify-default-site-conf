4673b67f3d04d42d30f3d06dc366521a9c60266a
wjcarey <contact@wjcarey.io>
2020-10-28 11:31:38 -0600

    init file upload

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # apache2-modify-default-site-conf
+
+
+### run this code from the commandline. The script will ask you where to install wordpress files. You can also pass in the path as an argument
+~~~
+sudo curl -o apache2-modify-default-site-conf.sh https://raw.githubusercontent.com/wjcarey/apache2-modify-default-site-conf/main/apache2-modify-default-site-conf.sh && sudo chmod 777 apache2-modify-default-site-conf.sh && sudo ./apache2-modify-default-site-conf.sh
+~~~
---
+++ apache2-modify-default-site-conf.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+if [ -z "$1" ]
+    then
+        read INSTALL_PATH
+        echo -e "\e[32mconfirm defaut site path to ${INSTALL_PATH}, Do you want to continue? [Y/n] \e[39m"
+        read CONFIRM_INSTALL_PATH
+    else
+    INSTALL_PATH=${2}
+    CONFIRM_INSTALL_PATH="Y"
+fi
+
+INSTALL_PATH=$(realpath -s --canonicalize-missing $INSTALL_PATH)
+
+if [ "$CONFIRM_INSTALL_PATH" != "${CONFIRM_INSTALL_PATH#[Yy]}" ] ;then
+    echo "updating apache2 default virtual host for directory ${INSTALL_PATH} ..."
+    sed -i "s/^Document Root.*/Document Root ${INSTALL_PATH}/" /etc/apache2/sites-available/000-default.conf
+    echo "restarting apache2 ..."
+    systemctl restart apache2
+else
+    echo "notice: configuration of default site was skipped by user..."
+fi
+
+#SELF DELETE AND EXIT
+rm -- "$0"
+exit
